78af34cbb0082f471c9a29bb774b610c40d8b41c
Misbah58 <afsarmisbah@gmail.com>
2023-01-14 21:11:52 -0500

    class1

---
+++ Variables.java
@@ -0,0 +1,9 @@
+package class1;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("i have learnt some of the rules of java");
+        System.out.println("Another line");
+    }
+}
+
